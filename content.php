19e012621f0a7825457f35c9d8797902710ca359
YoArts <hi@yoarts.com>
2014-05-12 21:05:29 +0700

    Fix bugs

--- content.php
+++ content.php
@@ -1,4 +1,5 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php $post_class = has_post_thumbnail() ? 'has-thumbnail' : ''; ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class($post_class); ?>>
 	<?php if(has_post_thumbnail()) : ?>
 		<div class="entry-thumbnail">
 			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail(); ?></a>
